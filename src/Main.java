6f2a3796f893a97782c666bd3d407c8e2d7e28b0
Chris Spencer <cvspencer@gmail.com>
2018-03-11 23:28:33 +0900

    Trying a different method using IvParameterSpec
    Eliminated padding error
    
    Now decryption doesn't work

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.Cipher;
 import java.security.Key;
@@ -11,55 +12,95 @@ public class Main {
     public static void main(String args[]) {
 
         {
-            String in = "Hello";
-            String enc;
-            String dec;
+            String in = "Hello????";
+            byte[] enc;
+            byte[] dec;
 
-            enc = encrypt(in, encryptionKey);
+            enc = encrypt(in);
 
-            dec = decrypt(enc, encryptionKey);
+            System.out.println(enc);
+
+            dec = decrypt(enc);
 
             System.out.println(dec);
         }
     }
 
 
+//
+//    // ENCRYPT
+//    public static String encrypt(String strClearText, String encryptionKey) {
+//        String strData = "";
+//
+//        try {
+//            Key key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
+//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+//            cipher.init(Cipher.ENCRYPT_MODE, key);
+//            byte[] encryptedText = cipher.doFinal(strClearText.getBytes());
+//
+//            strData = new String(encryptedText);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return strData;
+//    }
+//
+//
+//    // DECRYPT
+//    public static String decrypt(String strEncrypted, String encryptionKey) {
+//        String strData = "";
+//
+//        try {
+//            Key key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
+//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+//            cipher.init(Cipher.DECRYPT_MODE, key);
+//
+//            strData = new String(cipher.doFinal(strEncrypted.getBytes()));
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return strData;
+//    }
 
-    // ENCRYPT
-    public static String encrypt(String strClearText, String encryptionKey) {
-        String strData = "";
-
+    public static byte[] encrypt(String plainTextString) {
+        byte[] encrypted = null;
         try {
-            Key key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encryptedText = cipher.doFinal(strClearText.getBytes());
 
-            strData = new String(encryptedText);
+            Key skeySpec = new SecretKeySpec(encryptionKey.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] iv = new byte[cipher.getBlockSize()];
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            IvParameterSpec ivParams = new IvParameterSpec(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec,ivParams);
+            encrypted  = cipher.doFinal(plainTextString.getBytes());
+            System.out.println("encrypted string size:" + encrypted.length);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        return strData;
+        return encrypted;
     }
 
-
-    // DECRYPT
-    public static String decrypt(String strEncrypted, String encryptionKey) {
-        String strData = "";
-
+    public static  byte[]  decrypt(byte[] encryptedBytes) {
+        byte[] original = null;
+        Cipher cipher = null;
         try {
+
             Key key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            //the block size (in bytes), or 0 if the underlying algorithm is not a block cipher
+            byte[] ivByte = new byte[cipher.getBlockSize()];
+            //This class specifies an initialization vector (IV). Examples which use
+            //IVs are ciphers in feedback mode, e.g., DES in CBC mode and RSA ciphers with OAEP encoding operation.
+            IvParameterSpec ivParamsSpec = new IvParameterSpec(ivByte);
+            cipher.init(Cipher.DECRYPT_MODE, key, ivParamsSpec);
+            original= cipher.doFinal(encryptedBytes);
 
-            strData = new String(cipher.doFinal(strEncrypted.getBytes()));
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        return strData;
+        return original;
     }
-
-
 }
